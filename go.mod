7ca646ed1d85df03777260b9d6c48cb653b20cc3
Tyler Lafayette <tylerlafayette@gmail.com>
2020-06-18 00:20:22 -0700

    Init authentication service + add dependency to go mod

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/joinimpact/api
 go 1.14
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.4.1
 	github.com/go-chi/chi v4.1.1+incompatible
 	github.com/jinzhu/gorm v1.9.12
 	github.com/rs/zerolog v1.18.0
---
+++ internal/authentication/service.go
@@ -0,0 +1,9 @@
+package authentication
+
+// Service represents a provider of authentication services.
+type Service interface {
+}
+
+// service represents the default authentication service of this package.
+type service struct {
+}
