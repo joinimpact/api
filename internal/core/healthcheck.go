ee7fb93906111e0a333fddaa652a607c6f8bd057
Tyler Lafayette <tylerlafayette@gmail.com>
2020-06-19 10:38:33 -0700

    Move healthcheck to /healthcheck

--- internal/core/healthcheck.go
+++ internal/core/healthcheck.go
@@ -1,8 +1,14 @@
 package core
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // healthcheckHandler is a handler that returns 200 to signify that the API is in good health for receiving requests.
 func healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	// Print a debug message.
+	fmt.Println("OK")
+
 	w.WriteHeader(http.StatusOK)
 }
