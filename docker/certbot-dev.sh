26e3d136be5ca869b421f81db9175bd34a4c86c8
Oleg Datsko <umunin@gmail.com>
2020-06-19 19:24:29 +0300

    postgres healthcheck + certbot script

--- docker/certbot-dev.sh
+++ docker/certbot-dev.sh
@@ -6,7 +6,5 @@ docker run -ti --rm --hostname=certbot --name=certbot \
               -v ${DATA_PATH}/joinimpact/certbot/log:/var/log/letsencrypt:rw \
               certbot/certbot \
               certonly --webroot -w /var/www/certbot \
-              --email <CHANGE_ME> \
               --rsa-key-size 2048 --agree-tos --force-renewal \
-              -d <CHANGE_ME>
-              # --dry-run
+              -d dev.joinimpact.org
