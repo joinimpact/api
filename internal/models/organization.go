2dcb946cb2ae1004f0ef6a31ee79951bcb3c5256
Tyler Lafayette <tylerlafayette@gmail.com>
2020-06-26 00:25:50 -0700

    Docs for organizations endpoints

--- internal/models/organization.go
+++ internal/models/organization.go
@@ -6,7 +6,9 @@ type Organization struct {
 	Active         bool                       `json:"-"`              // controls whether or not the organization is active
 	CreatorID      int64                      `json:"creatorId"`      // the organization's creator's ID (User)
 	Name           string                     `json:"name"`           // the organization's name
+	Description    string                     `json:"description"`    // a description of the organization
 	ProfilePicture string                     `json:"profilePicture"` // the organization's profile picture/logo
 	WebsiteURL     string                     `json:"websiteUrl"`     // the organization's website's URL
-	ProfileFields  []OrganizationProfileField `json:"profile"`        // fields of the organization's profile
+	Location       string                     `json:"location"`
+	ProfileFields  []OrganizationProfileField `json:"profile"` // fields of the organization's profile
 }
