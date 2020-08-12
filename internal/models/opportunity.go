c17c98d42d9e518cc3e45ab25e5e611ca3c14aa2
Tyler Lafayette <tylerlafayette@gmail.com>
2020-08-12 00:07:19 -0700

    Optimize bulk find volunteer opportunities operations

--- internal/models/opportunity.go
+++ internal/models/opportunity.go
@@ -5,21 +5,26 @@ import "context"
 // Opportunity represents an organization's opportunity.
 type Opportunity struct {
 	Model
-	Active         bool         `json:"-"`                             // when false, the opportunity will be treated as if deleted. Useful for suspensions, etc.
-	OrganizationID int64        `json:"opportunityId"`                 // the id of the parent organization
-	Organization   Organization `json:"-"`                             //
-	CreatorID      int64        `json:"creatorId"`                     // the id of the user who created the opportunity initially
-	Creator        User         `json:"-" gorm:"foreignkey:CreatorID"` //
-	Public         bool         `json:"public"`                        // whether or not the opportunity should be shown to volunteers
-	Title          string       `json:"title"`                         // the title of the opportunity
-	ProfilePicture string       `json:"profilePicture"`                // a url to the opportunity's banner image
-	Description    string       `json:"description"`                   // a long description of the opportunity and its purpose
+	Active                  bool                     `json:"-"`                             // when false, the opportunity will be treated as if deleted. Useful for suspensions, etc.
+	OrganizationID          int64                    `json:"opportunityId"`                 // the id of the parent organization
+	Organization            Organization             `json:"-"`                             //
+	CreatorID               int64                    `json:"creatorId"`                     // the id of the user who created the opportunity initially
+	Creator                 User                     `json:"-" gorm:"foreignkey:CreatorID"` //
+	Public                  bool                     `json:"public"`                        // whether or not the opportunity should be shown to volunteers
+	Title                   string                   `json:"title"`                         // the title of the opportunity
+	ProfilePicture          string                   `json:"profilePicture"`                // a url to the opportunity's banner image
+	Description             string                   `json:"description"`                   // a long description of the opportunity and its purpose
+	OpportunityTags         []OpportunityTag         `json:"-"`
+	OpportunityRequirements *OpportunityRequirements `json:"-"`
+	OpportunityLimits       *OpportunityLimits       `json:"-"`
 }
 
 // OpportunityRepository represents a repository of opportunities.
 type OpportunityRepository interface {
 	// FindByID finds a single entity by ID.
 	FindByID(ctx context.Context, id int64) (*Opportunity, error)
+	// FindByIDs finds multiple entities by an array of IDs.
+	FindByIDs(ctx context.Context, ids []int64) ([]Opportunity, error)
 	// FindByOrganizationID finds multiple entities by the organization ID.
 	FindByOrganizationID(ctx context.Context, organizationID int64) ([]Opportunity, error)
 	// FindByCreatorID finds multiple entities by the creator ID.
